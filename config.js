17c1532bf707a1b8bca4844f58d24c51e9f6c6f8
xiaotox500 <78307472+xiaotox500@users.noreply.github.com>
2021-02-03 15:32:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    "TOKEN" : "ODAyOTc3NTk1MTc1NjAwMTU4.YA3FKQ.64GkHGu-udn2Qn7NtSLWXqEyoC4",
-    "PREFIX" : "+"
+    "TOKEN" : "token,
+    "PREFIX" : "prefix"
 }
 
 module.exports = config;
